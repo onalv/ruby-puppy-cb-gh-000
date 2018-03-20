c4cc5355fc997f9f6626d1caef08301503af432b
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-20 20:38:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,16 @@
 class Dog
   @@all = []
   def initialize
+    @@all << self
+  end
 
+  def self.all
+    @all.each do |dog|
+      puts dog
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
   end
 end
